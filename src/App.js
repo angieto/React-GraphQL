7c0ebd97ec8bd035a9e8d60b549987d31bb257ce
Angie To <angie.to@berkeley.edu>
2020-06-15 23:17:21 -0700

    setup redux, applied change to user authentication

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import React from "react"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import Header from "./components/header/header"
 import HomePage from "./pages/home/homepage"
 import ShopPage from "./pages/shop/shop"
 import SignInNSignUpPage from "./pages/signin-n-signup/signin-n-signup"
 
+import { connect } from "react-redux"
+import { setCurrentUser } from "./redux/user/user-actions"
+
 import "./App.css"
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            currentUser: null,
-        }
-    }
-
     unsubscribeFromAuth = null
 
     componentDidMount() {
+        const { setCurrentUser } = this.props
         // setup an open subscription from firebase that tracks whenever the user's updated
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth)
                 userRef.onSnapshot((snapshot) => {
-                    this.setState({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data(),
-                        },
+                    setCurrentUser({
+                        id: snapshot.id,
+                        ...snapshot.data(),
                     })
                 })
             }
 
             // if user logs out
-            this.setState({ currentUser: userAuth })
+            setCurrentUser(userAuth)
         })
     }
 
@@ -46,15 +41,35 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header />
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/shop" component={ShopPage} />
-                    <Route path="/signin" component={SignInNSignUpPage} />>
+                    <Route
+                        exact
+                        path="/signin"
+                        render={() => {
+                            return this.props.currentUser ? (
+                                <Redirect to="/" />
+                            ) : (
+                                <SignInNSignUpPage />
+                            )
+                        }
+                            
+                        }
+                    />
                 </Switch>
             </div>
         )
     }
 }
 
-export default App
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import { auth } from "../../firebase/firebase.utils"
 
 import { ReactComponent as Logo } from "../../assets/crown.svg"
+import { connect } from "react-redux"
 
 import "./header.scss"
 
@@ -31,4 +32,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(Header)
