450620863b579d344aa87313bf932e92d34a5b8e
Angie To <angie.to@berkeley.edu>
2020-06-29 21:53:38 -0700

    update cart item count whenever an item's added

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -4,18 +4,24 @@ import { toggleCartHidden } from "../../redux/cart/cart-actions"
 import { connect } from "react-redux"
 
 import "./cart-icon.scss"
+import { selectCartItemsCount } from "../../redux/cart/cart-selectors"
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
+
+// use memoization (reselect library) to only update itemCount if the cartItem's changed
+const mapStateToProps = (cart) => ({
+    itemCount: selectCartItemsCount(cart),
+})
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
