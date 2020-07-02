f95fe15f27f43490880e6f7879f0ea772fde35e0
Angie To <angie.to@berkeley.edu>
2020-07-01 22:00:04 -0700

    add and remove cart item quantity

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -9,5 +9,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         )
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const targetItem = cartItems.find((item) => item.id === cartItemToRemove.id)
+
+    if (targetItem.quantity === 1) {
+        return cartItems.filter((item) => item.id !== cartItemToRemove.id)
+    }
+    
+    return cartItems.map((item) =>
+        item.id === cartItemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+    )
 }
