b242c8f60f23b2ae0b8c506303ffab049ad717bf
Angie To <angie.to@berkeley.edu>
2020-07-01 21:40:31 -0700

    clear item from cart

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -18,6 +18,13 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload),
             }
+        case CartActionTypes.CLEAR_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    (cartItem) => cartItem.id !== action.payload.id
+                ),
+            }
         default:
             return state
     }
