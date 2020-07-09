28a4c4cbb4b467d54bd6ea5f081b5fae9b2014bf
Angie To <angie.to@berkeley.edu>
2020-07-08 22:24:59 -0700

    get the corresponding collection from the param's collectionId, memoize the collection to prevent it from recalled again

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { connect } from "react-redux"
+import { selectCollection } from "../../redux/shop/shop-selectors"
 
 import "./collection.scss"
 
-const CollectionPage = ({ match }) => (
-    <div className="collection-page">
-        <h2>{match.params.categoryId}</h2>
-    </div>
-)
+const CollectionPage = ({ collection }) => {
+    return (
+        <div className="collection-page">
+            <h2>{collection.title}</h2>
+        </div>
+    )
+}
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+})
+
+export default connect(mapStateToProps)(CollectionPage)
--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,8 +1,27 @@
 import { createSelector } from "reselect"
+import memoize from "lodash.memoize"
+
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    women: 4,
+    men: 5,
+}
 
 const selectShop = (state) => state.shop
 
 export const selectCollections = createSelector(
     [selectShop],
     (shop) => shop.collections
+)
+
+// memoize will just return the stored selector if the same collectionUrlParam's passed in again
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector([selectCollections], (collections) =>
+        collections.find(
+            (collection) =>
+                collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+        )
+    )
 )
